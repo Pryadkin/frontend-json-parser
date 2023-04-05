ad77c1f33aa17820a9231585f164399b77d93e22
Anton Pryadkin <ap.pryadkin@dm-solutions.ru>
2023-04-05 14:44:18 +0600

    update updateObjToTree.test.js

--- src/helpers/updateObjToTree.test.ts
+++ src/helpers/updateObjToTree.test.ts
@@ -2,24 +2,22 @@ import {updateObjForTree} from './updateObjToTree'
 
 import test1 from './treeForTests/test_1.json'
 import test1Update from './treeForTests/test_1_update.json'
-import test3 from './treeForTests/test_3.json'
-import test3Update from './treeForTests/test_3_update.json'
+import test2 from './treeForTests/test_2.json'
+import test2Update from './treeForTests/test_2_update.json'
+// import test3 from './treeForTests/test_3.json'
+// import test3Update from './treeForTests/test_3_update.json'
 
 describe('checking update object for tree', () => {
     test('to equal correct value', () => {
         const treeObjectJson = JSON.stringify(updateObjForTree((test1 as any)))
         const updateTreeObjJson = JSON.stringify(test1Update)
-        console.log(treeObjectJson)
-        console.log(updateTreeObjJson)
 
         expect(treeObjectJson)
             .toEqual(updateTreeObjJson)
     })
     test('to equal correct value', () => {
-        const treeObjectJson = JSON.stringify(updateObjForTree((test3 as any)))
-        console.log('555', treeObjectJson)
-        const updateTreeObjJson = JSON.stringify(test3Update)
-        console.log('5--', updateTreeObjJson)
+        const treeObjectJson = JSON.stringify(updateObjForTree((test2 as any)))
+        const updateTreeObjJson = JSON.stringify(test2Update)
 
         expect(treeObjectJson)
             .toEqual(updateTreeObjJson)
